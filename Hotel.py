583a465a7fe607b9114fcb63700b227343e2dcbb
Chetan-Tiwari-2430 <chetantiwari2430@gmail.com>
2026-07-20 19:50:59 +0530

    The Room Availability checker feaure added

--- Hotel.py
+++ Hotel.py
@@ -1,3 +1,12 @@
 class Hotel:
     is_room_available = [False,False,False,True,True]
     
+    # Here we create a method that checks the room is available or not
+    def room_availability_checker(self):
+        for i in range(len(self.is_room_available)):
+            if(self.is_room_available[i] == True):
+                self.is_room_available[i] = False
+                return True
+        return False
+    
+
