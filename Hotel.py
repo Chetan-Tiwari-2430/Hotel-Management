a8dbc2d932736dcced279e7ebe1b3c62b11ef812
Chetan-Tiwari-2430 <chetantiwari2430@gmail.com>
2026-07-20 20:10:35 +0530

    Some small chages and add the non ac checker methos

--- Hotel.py
+++ Hotel.py
@@ -1,11 +1,22 @@
 class Hotel:
-    is_room_available = [False,False,False,True,True]
+    ac_room_available = [False,False,False,True,True]
+    non_ac_room_available = [False,True,True,True,False]
     
-    # Here we create a method that checks the room is available or not
-    def room_availability_checker(self):
-        for i in range(len(self.is_room_available)):
-            if(self.is_room_available[i] == True):
-                self.is_room_available[i] = False
+    # Here we create a method that checks the Ac rooms is available or not
+    def ac_room_availability_checker(self):
+        # This loop is check the room is available or not if Ac rooms is available !
+        # The next process is continue
+        for i in range(len(self.ac_room_available)):
+            if(self.ac_room_available[i] == True):
+                self.ac_room_available[i] = False
+                return True
+        return False
+    
+    def non_ac_room_availability_checker(self):
+        #This loop the Availability of the non Ac rooms
+        for i in range(len(self.non_ac_room_available)):
+            if(self.non_ac_room_available[i] == True):
+                self.non_ac_room_available[i] = False
                 return True
         return False
     
